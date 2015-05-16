12f1b4bcd66347643b8e96134cd6b7a89655204b
Michael Wiss <michael.wiss@gmail.com>
2015-05-16 17:01:49 -0500

    .

--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -2,7 +2,7 @@ def project_attributes(overrides = {})
 	{
      name:  "Water Fund",
      description: "Draught relief",
-     target_pledge_amount: 300000.00,
+     target_pledge_amount: 300000000.00,
      pledging_ends_on: "2016-11-15",
      website: "http://www.charitywater.org/"
      }.merge(overrides)
