36cf3f672776b606487607408175c78bd200e590
Michael Wiss <michael.wiss@gmail.com>
2015-05-17 17:31:48 -0500

    added expectations to show_project spec

--- spec/features/show_project_spec.rb
+++ spec/features/show_project_spec.rb
@@ -17,5 +17,21 @@ describe "Viewing an individual project" do
        expect(page).to have_text(project.website)
    end
 
+   it "shows the days remaining if the pledging end date is in the future" do
+     project = Project.create(project_attributes(pledging_ends_on: 1.day.from_now))
+
+     visit project_url(project)
+
+     expect(page).to have_text("1 day remaining")
+   end
+
+  it "shows 'All Done!' if the pledging end date is in the past" do
+    project = Project.create(project_attributes(pledging_ends_on: 2.days.ago))
+
+    visit project_url(project)
+
+    expect(page).to have_text("All Done!")
+  end
+
 
 end
