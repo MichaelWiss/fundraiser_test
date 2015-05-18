d1a5d7b35304b60506cce5e2b40b5675f6fc516c
Michael Wiss <michael.wiss@gmail.com>
2015-05-17 21:22:46 -0500

    finding bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
  get "projects" => "projects#index"
  get "projects/:id" => "projects#show", as: "project"
+ "projects#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,19 +19,5 @@ Project.create!([
      target_pledge_amount: 400000,
      website: "http://www.foodforthepoor.org/",
      pledging_ends_on: "2015-12-15",
-    },
-    {
-     name: "Water Fund",
-     description: "Draught Relief",
-     target_pledge_amount: 300000,
-     website: "http://www.charitywater.org/",
-     pledging_ends_on: "2016-11-15",
-    },
-    {
-     name: "Water Fund",
-     description: "Draught Relief",
-     target_pledge_amount: 300000,
-     website: "http://www.charitywater.org/",
-     pledging_ends_on: "2016-11-15",
-    },
+    }
 ])
