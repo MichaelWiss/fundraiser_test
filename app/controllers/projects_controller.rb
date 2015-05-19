f0ebd6ba8bfbc1cdbf1678bb2c0bb4fe1d69acbe
Michael Wiss <michael.wiss@gmail.com>
2015-05-19 14:50:16 -0500

    edit form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,8 @@ class ProjectsController < ApplicationController
 		@project = Project.find(params[:id])
 	end
 
+	def edit 
+		@project = Project.find(params[:id])
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  get "projects" => "projects#index"
  get "projects/:id" => "projects#show", as: "project"
- "projects#index"
+ get "projects/:id/edit" => "projects#edit", as: "edit_project"
+ root "projects#index"
+
 end
